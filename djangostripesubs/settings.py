cd65dbdd9e76a21f04d1cf3e86d0ad5318784ff8
duplxey <nick.tomazic@gmail.com>
2020-09-16 00:31:01 +0200

    Started Customers app + hooked it up.

--- djangostripesubs/settings.py
+++ djangostripesubs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'customers.apps.CustomersConfig',
     'subscriptions.apps.SubscriptionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- djangostripesubs/urls.py
+++ djangostripesubs/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('customers.urls')),
     path('', include('subscriptions.urls')),
 ]
