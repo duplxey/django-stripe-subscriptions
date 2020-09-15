ef03d48275e2ae11114601fc423ae1897da30a47
duplxey <nick.tomazic@gmail.com>
2020-09-16 00:29:18 +0200

    StripeCustomer.

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class StripeCustomer(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    stripeCustomerId = models.IntegerField(null=False, blank=False)
